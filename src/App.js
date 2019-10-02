57b6dde4a0df99706a9cdc326a8576d0f1a4bc68
Luke Dunscombe <ldunscombe@leantechniques.com>
2019-10-02 17:31:40 -0500

    Kicking off build

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-          Learn React
+          Learn React yo
                 </a>
             </header>
         </div>
